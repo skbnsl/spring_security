ee48623bd07bfddb299ecdaa9178b83bf4c94f77
Sagar Bansal <bansalsagar13@gmail.com>
2024-10-17 22:25:13 +0530

    some changes commit

--- src/main/java/com/security/learn/SecurityConfig.java
+++ src/main/java/com/security/learn/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.security.learn;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig {
 
